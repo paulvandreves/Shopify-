5eec4ea70be3729243d00aaf1f13dc1cc67ec47d
Paul  Van Dyke <gboss0352@gmail.com>
2018-06-07 08:46:16 -0600

    shopping edit

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient {
+    constructor(public name: string, public amount: number){}
+}
+
+
+
+// short hand version above
+//     public name: string;
+//     public amount: number;
+//     constructor(name: string, amount: number){
+//         this.name = name; 
+//         this.amount = amount; 
+//     }
