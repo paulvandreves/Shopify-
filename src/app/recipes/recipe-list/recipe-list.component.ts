49d91295fdb651cf80b92d33ede5ba14ce66fdfa
Paul  Van Dyke <gboss0352@gmail.com>
2018-08-10 11:16:03 -0400

    navigation with ngIF added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,14 @@ import {Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is Simply a test', 'https://4tbarallnaturalbeef.com/wp-content/uploads/2016/11/natural-beef-ribeye-steak.jpg')
+    new Recipe('A test recipe', 'This is Simply a test', 'https://4tbarallnaturalbeef.com/wp-content/uploads/2016/11/natural-beef-ribeye-steak.jpg'),
+
+    new Recipe('Another test recipe', 'This is another test', 'https://images-na.ssl-images-amazon.com/images/I/615zwvTSuzL.jpg' )
   ]; 
+
+  
+    
+  
 
   constructor() { }
 
